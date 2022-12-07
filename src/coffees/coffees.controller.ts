d0a9a7b0720093ecf603286e341cff3195255f87
Stephen Beattie <beattie19@hotmail.co.uk>
2022-12-07 15:59:10 +1100

    Add post request and use @Body to return body

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -11,5 +11,15 @@ export class CoffeesController {
   @Get('flavours')
   flavours() {
     return 'all flavours';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
