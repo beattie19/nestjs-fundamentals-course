f54b0e5a64d9333b7910d6e3a51fadae59316bcf
Stephen Beattie <beattie19@hotmail.co.uk>
2022-12-07 16:34:21 +1100

    Implement patch and delete actions

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
 
   // to create a nested url we can give the @Get decorator a name as a string
@@ -19,7 +19,18 @@ export class CoffeesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `this action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `this action removes #${id} coffee`;
   }
 }
