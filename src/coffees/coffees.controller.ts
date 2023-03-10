85dc358fefd22daf91328bf618f86d612d778383
Stephen Beattie <beattie19@hotmail.co.uk>
2023-03-10 23:24:30 +1100

    Diving into e2e

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -16,14 +16,14 @@ import {
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { SetMetadata } from '@nestjs/common';
-import { Public } from 'src/common/decorators/public.decorator';
-import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
-import { Protocol } from 'src/common/decorators/protocal.decorator';
+import { Public } from '../common/decorators/public.decorator';
+import { ParseIntPipe } from '../common/pipes/parse-int/parse-int.pipe';
+import { Protocol } from '../common/decorators/protocal.decorator';
 import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 // Applies to all routes
--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,6 +1,6 @@
-import { Coffee } from 'src/coffees/entities/coffee.entity';
-import { Flavor } from 'src/coffees/entities/flavor.entity';
-import { CoffeeRefactor1678235950925 } from 'src/migrations/1678235950925-CoffeeRefactor';
+import { Coffee } from './src/coffees/entities/coffee.entity';
+import { Flavor } from './src/coffees/entities/flavor.entity';
+import { CoffeeRefactor1678235950925 } from './src/migrations/1678235950925-CoffeeRefactor';
 import { DataSource } from 'typeorm';
 
 export default new DataSource({
